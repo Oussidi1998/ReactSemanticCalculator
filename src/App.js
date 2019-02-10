367d9bbb140541b561b17d2def69649015882a37
Oussidi1998 <osidi1998@gmail.com>
2019-02-10 23:49:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,106 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Card,Grid,Form } from 'semantic-ui-react'
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class Interface extends Component {
+
+    state = {
+        expression:'',
+    }
+    
+    buttonPressed = (e) =>{
+        switch(e.target.name){
+            case 'number':if(this.state.expression === "0"){
+                                this.setState({
+                                    expression:e.target.value
+                                });
+                            }else{
+                                this.setState({
+                                    expression:this.state.expression+e.target.value
+                                });
+                            }break;
+            case 'operation':if(this.state.expression !== ""){
+                                if(typeof(this.state.expression) === 'number'){
+                                    this.setState({
+                                        expression:this.state.expression+e.target.value
+                                    });
+                                }else{
+                                    let lastChar = this.state.expression.charAt(this.state.expression.length - 1);
+                                if(lastChar !== "+" && lastChar !== "-" && lastChar !== "/" && lastChar !== "*"){
+                                    this.setState({
+                                        expression:this.state.expression+e.target.value
+                                    });
+                                }
+                                }
+                            }else{
+                                this.setState({
+                                    expression:'syntax error'
+                                });
+                            }break;
+            
+            case 'delete': this.setState({
+                expression:'',
+            });break;
+
+            case 'egal': if(this.state.expression !== ""){
+                            let lastChar = this.state.expression.charAt(this.state.expression.length - 1);
+                            if(lastChar !== "+" && lastChar !== "-" && lastChar !== "/" && lastChar !== "*"){
+                                this.setState({
+                                    expression:eval(this.state.expression)
+                                });
+                            }
+                        }break;
+            
+
+            default: this.setState({
+                expression:'there was an error'
+            })
+
+        }
+    }
+
+    render() {
+        return (
+            <div className='interface'>
+                <Grid centered columns={4}>
+                <Grid.Column>
+                    <Card>
+                        <Card.Content header='React Semantic UI Calculator' />
+                        <Card.Content extra>
+                            <Form>
+                                <Form.Input fluid placeholder='' value={this.state.expression} readOnly />
+                            </Form><br/>
+                            <div className='btnGroup'>
+                                <button name='number' value='7' onClick={this.buttonPressed} >7</button>
+                                <button name='number' value='8' onClick={this.buttonPressed} >8</button>
+                                <button name='number' value='9' onClick={this.buttonPressed} >9</button>
+                                <button name='operation' value='+' onClick={this.buttonPressed} >+</button>
+                            </div>
+                            <div className='btnGroup'>
+                                <button name='number' value='4' onClick={this.buttonPressed} >4</button>
+                                <button name='number' value='5' onClick={this.buttonPressed} >5</button>
+                                <button name='number' value='6' onClick={this.buttonPressed} >6</button>
+                                <button name='operation' value='-' onClick={this.buttonPressed} >-</button>
+                            </div>
+                            <div className="btnGroup">
+                                <button name='number' value='1' onClick={this.buttonPressed} >1</button>
+                                <button name='number' value='2' onClick={this.buttonPressed} >2</button>
+                                <button name='number' value='3' onClick={this.buttonPressed} >3</button>
+                                <button name='operation' value='*' onClick={this.buttonPressed} >*</button>
+                            </div>
+                            <div className="btnGroup">
+                                <button name='delete' value='C' onClick={this.buttonPressed} >C</button>
+                                <button name='number' value='0' onClick={this.buttonPressed} >0</button>
+                                <button name='egal' value='=' onClick={this.buttonPressed} >=</button>
+                                <button name='operation' value='/' onClick={this.buttonPressed} >/</button>
+                            </div>
+                        </Card.Content>
+                    </Card>
+                </Grid.Column>
+            </Grid>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default Interface;
